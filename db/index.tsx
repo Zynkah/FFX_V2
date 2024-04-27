9892f018b65a9f51fc5e24d9f8c86a22e1806391
Zynkah <ffgurl07@yahoo.com>
2024-04-26 21:56:39 -0500

    feat: setup

--- db/index.tsx
+++ db/index.tsx
@@ -1,15 +1,25 @@
-// import postgres from "postgres";
+import { Pool } from "pg";
 
-// async function getData() {
-//   const sql = postgres(process.env.DATABASE_URL, { ssl: "require" });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
 
-//   const response = await sql`SELECT version()`;
+  ssl: true,
+});
 
-//   console.log(response);
+async function getData() {
+  const client = await pool.connect();
 
-//   return response;
-// }
+  try {
+    const response = await client.query("SELECT version()");
 
-// export default async function Page() {
-//   const data = await getData();
-// }
+    console.log(response.rows[0]);
+
+    return response.rows[0];
+  } finally {
+    client.release();
+  }
+}
+
+export default async function Page() {
+  const data = await getData();
+}
