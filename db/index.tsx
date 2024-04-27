dbda0872dcd471f9096ac5e5ae23e942bbe28c73
Zynkah <ffgurl07@yahoo.com>
2024-04-26 21:43:34 -0500

    feat: db setup

---
+++ db/index.tsx
@@ -0,0 +1,15 @@
+// import postgres from "postgres";
+
+// async function getData() {
+//   const sql = postgres(process.env.DATABASE_URL, { ssl: "require" });
+
+//   const response = await sql`SELECT version()`;
+
+//   console.log(response);
+
+//   return response;
+// }
+
+// export default async function Page() {
+//   const data = await getData();
+// }
