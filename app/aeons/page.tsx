41398718012dae5755d63351857b405ed6fb0d27
Zynkah <ffgurl07@yahoo.com>
2024-05-10 13:03:51 -0500

    fix: renames and nav fixed

--- app/aeons/page.tsx
+++ app/aeons/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
-import BasicCardLayout from "@/components/basic-card-layout";
+import BasicCardLayout from "@/components/clickable-card-layout";
 import Image from "next/image";
 import Link from "next/link";
 import { motion } from "framer-motion";
