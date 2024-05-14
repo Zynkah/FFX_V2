3debc21537a19df4a5a86f88c25940636210b774
Zynkah <ffgurl07@yahoo.com>
2024-05-14 12:30:49 -0500

    Feat: pages added

--- components/players-table.tsx
+++ components/players-table.tsx
@@ -1,21 +1,36 @@
 import { TableCell, TableRow } from "@/components/ui/table";
+import Image from "next/image";
 
 interface PlayerProps {
   name: string;
   location: string;
   techniques: string;
-  children: React.ReactNode;
+  src: string;
+  width: number;
+  height: number;
+  alt: string;
 }
 
 export default function PlayersTable({
   name,
   location,
   techniques,
-  children,
+  src,
+  width,
+  height,
+  alt,
 }: PlayerProps) {
   return (
     <TableRow>
-      <TableCell className="font-medium">{children}</TableCell>
+      <TableCell className="font-medium">
+        <Image
+          src={src}
+          width={width}
+          height={height}
+          alt={alt}
+          className="flex justify-center items-center m-auto rounded-lg object-scale-down"
+        />
+      </TableCell>
       <TableCell>{name}</TableCell>
       <TableCell>{location}</TableCell>
       <TableCell className="text-right">{techniques}</TableCell>
